07151b68a0c5d643daa1ccbe23bbd7eb9c43b4fa
=afiganteng <=nurafiat27@gmail.com>
2021-06-18 02:36:11 +0700

    first commit

---
+++ database/migrations/2021_05_03_120639_create_mahasiswas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nama',40);
+            $table->integer('Kelas',1);
+            $table->string('Jurusan',40);
+            $table->integer('No_handphone',13);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
